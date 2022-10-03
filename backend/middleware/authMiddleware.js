ff1b5041c9a134e73d7e9210c41ff94c541aaf2a
DarGrz <102190066+DarGrz@users.noreply.github.com>
2022-10-03 18:54:41 +0200

    Bearer token added

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,9 +1,19 @@
 import jwt from "jsonwebtoken";
+import expressAsyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 
 const protect = async (req, res, next) => {
   let token;
-  console.log(req.header.authorization);
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    console.log("token found");
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, not token");
+  }
   next();
 };
 
